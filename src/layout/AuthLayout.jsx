57e8a08d92dfd8d45cc66b587f8fc9cdbad6822e
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-23 20:33:39 -0600

    Primero estilos a el LOGIN

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -5,9 +5,10 @@ import { Outlet } from 'react-router-dom';
 const AuthLayout = () => {
   return (
     <>
-        <h1>ADMINISTRADOR DE PACIENTES DE VETERINARIA</h1>
-
+    <main className='container mx-auto md:grid md:grid-cols-2 mt-12'>  
         <Outlet />
+    </main>
+
     </>
   )
 }
