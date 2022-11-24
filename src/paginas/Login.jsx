ab00df5f53d4adae1b2c3e8dd2c0418cb9f2f7ae
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-23 23:02:21 -0600

    Creando pagina de OLVIDE CONTRASEÑA

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
                  <span className="text-black"> Pacientes</span>
             </h1>
         </div>
-        <div>
+        <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
             <form>
                 <div className="my-5">
                     <label
