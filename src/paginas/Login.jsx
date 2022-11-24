cb4d688f931acf4bb88a167ce8c733018c227f8a
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-23 20:49:28 -0600

    Creando el formulario para el LOGIN

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -9,7 +9,40 @@ const Login = () => {
             </h1>
         </div>
         <div>
-            2
+            <form>
+                <div className="my-5">
+                    <label
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                    >
+                        Email
+                    </label>
+                    <input 
+                        type="email"
+                        placeholder="Email de registro"
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    />
+                </div>
+                <div className="my-5">
+                    <label
+                        className="uppercase text-gray-600 block text-xl font-bold"
+                    >
+                        Password
+                    </label>
+                    <input 
+                        type="password"
+                        placeholder="Ingresa un Password"
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    />
+                </div>
+
+                <input 
+                    type="submit"
+                    value="Iniciar Sesión"
+                    className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                />
+
+
+            </form>
         </div>
     </>
   )
