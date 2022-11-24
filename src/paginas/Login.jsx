1564a1826ed5f199348e5fe77a81b296f0703c30
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-23 21:01:08 -0600

    Añadiendo ROUTING hacia REGISTRO y OLVIDE CONTRASEÑA

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,3 +1,5 @@
+//importar componente para detener el pqueño spiner al ir a otro navegador
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
@@ -43,6 +45,17 @@ const Login = () => {
 
 
             </form>
+
+            <nav className='mt-10 lg:flex lg:justify-between'>
+                <Link 
+                    className='block text-center my-5 text-gray-500'
+                    to="/registrar">¿No tienes una cuenta? Registrate</Link>
+
+                <Link 
+                    className='block text-center my-5 text-gray-500'
+                    to="/olvide-password">Olvide mi Password</Link>
+            </nav>
+
         </div>
     </>
   )
