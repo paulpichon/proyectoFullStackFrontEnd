1b0c6060b27c27216faf6186e13b24abfb3d9d9d
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-24 20:49:55 -0600

    Confirmando la cuenta del usuario

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+//se quito el Strict Mode <React.StrictMode>
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 )
