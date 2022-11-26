c67f2ca0b8e217df1f251c9f2bcf9c9f3e88ba02
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-26 00:35:38 -0600

    Autenticacion de usuarios - Primeros pasos

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -1,8 +1,47 @@
+import { useState } from 'react';
 //importar componente para detener el pqueño spiner al ir a otro navegador
 import { Link } from 'react-router-dom';
+import Alerta from '../components/Alerta';
 import useAuth from '../hooks/useAuth';
+import clienteAxios from '../config/axios';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [alerta, setAlerta] = useState({});
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        //si alguno de los cmapos esta vacio
+        if ([email, password].includes('')) {
+            
+            setAlerta({
+                msg: 'Todos los cmapos son obligatorios',
+                error: true
+            });
+
+            return;
+        }
+
+        try {
+            const {data} = await clienteAxios.post('/veterinarios/login', {
+                email,
+                password
+            });
+            //local storage
+            localStorage.setItem('token', data.token);
+
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            });
+        }
+    }
+
+    //alerta
+    const {msg} = alerta;
 
   return (
     <>
@@ -13,7 +52,14 @@ const Login = () => {
             </h1>
         </div>
         <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-            <form>
+
+            { msg && 
+                <Alerta 
+                    alerta={alerta}
+                />
+            }
+
+            <form onSubmit={handleSubmit}>
                 <div className="my-5">
                     <label
                         className="uppercase text-gray-600 block text-xl font-bold"
@@ -24,6 +70,8 @@ const Login = () => {
                         type="email"
                         placeholder="Email de registro"
                         className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        value={email}
+                        onChange={ e => setEmail(e.target.value) }
                     />
                 </div>
                 <div className="my-5">
@@ -36,6 +84,8 @@ const Login = () => {
                         type="password"
                         placeholder="Ingresa un Password"
                         className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        value={password}
+                        onChange={ e => setPassword(e.target.value) }
                     />
                 </div>
 
