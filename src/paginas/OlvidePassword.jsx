53398aaa6a35c0365cea9fa8d5fd4ecf8841e5c7
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-25 19:13:37 -0600

    Generar TOKEN y VALIDACION de TOEKN UNICO para cambiar el PASSWORD

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,6 +1,39 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Alerta from '../components/Alerta';
+import clienteAxios from '../config/axios';
 
 const OlvidePassword = () => {
+
+    const [email, setEmail] = useState('');
+    const [alerta, setAlerta] = useState({});
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        //verificar que haya algio en el INPUT
+        if (email === '' || email.length < 6) {
+            setAlerta({msg: 'El Email es obligatorio', error: true});
+            return;
+        }
+
+        try {
+            const { data } = await clienteAxios.post('/veterinarios/olvide-password', {email});
+
+            //mensaje
+            setAlerta({msg: data.msg});
+
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            });
+        }
+
+    };
+
+    const { msg } = alerta;
+
   return (
     <>
         <div>
@@ -11,7 +44,12 @@ const OlvidePassword = () => {
         </div>
 
         <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-            <form>
+
+            { msg && <Alerta 
+                alerta={alerta}
+            />}
+
+            <form onSubmit={handleSubmit}>
                 <div className="my-5">
                     <label
                         className="uppercase text-gray-600 block text-xl font-bold"
@@ -22,6 +60,8 @@ const OlvidePassword = () => {
                         type="email"
                         placeholder="Email de registro"
                         className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        value={email}
+                        onChange={e=> setEmail(e.target.value)}
                     />
                 </div>
 
