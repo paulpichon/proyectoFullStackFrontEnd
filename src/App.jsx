3bea39d7a5266fa964a29645c09851871f25040a
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-29 20:38:16 -0600

    Creando un CONTEXT de PACIENTES

--- src/App.jsx
+++ src/App.jsx
@@ -15,27 +15,28 @@ import NuevoPassword  from "./paginas/NuevoPassword";
 import AdministrarPacientes from './paginas/AdministrarPacientes';
 
 import { AuthProvider } from './context/AuthProvider';
+import { PacientesProvider } from './context/PacientesProvider';
 
 function App() {
 
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-            <Route path="registrar" element={<Registrar />} />
-            <Route path="olvide-password" element={<OlvidePassword />} />
-            <Route path="olvide-password/:token" element={<NuevoPassword />} />
-            <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
-          </Route>
+        <PacientesProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route path="olvide-password/:token" element={<NuevoPassword />} />
+              <Route path="confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
 
-          <Route path="/admin" element={<RutaProtegida />}>
-            <Route index element={<AdministrarPacientes />}  />
-          </Route>
-
-
-        </Routes>
+            <Route path="/admin" element={<RutaProtegida />}>
+              <Route index element={<AdministrarPacientes />}  />
+            </Route>
+          </Routes>
+        </PacientesProvider>
       </AuthProvider>  
     </BrowserRouter>
   )
