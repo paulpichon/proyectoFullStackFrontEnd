43dfa0c1ef4c263a9a6caf8be682ad0a3f582df1
Paul Pichon F <paul10_barca@hotmail.com>
2022-11-29 21:22:43 -0600

    Obtener Pacientes del usuario AUTENTICADO

--- src/context/PacientesProvider.jsx
+++ src/context/PacientesProvider.jsx
@@ -7,6 +7,33 @@ export const PacientesProvider = ({children}) => {
 
     const [pacientes, setPacientes] = useState([]);
 
+    useEffect(() => {
+        const obtenerPacientes = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                //si no hay token
+                if (!token) return;
+                //config para hacer la peticion
+                const config = {
+                    headers: {
+                        "Content-Type":"application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                };
+
+                const { data } = await clienteAxios('/pacientes', config);
+
+                setPacientes( data );
+
+
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        obtenerPacientes();
+    }, []);
+
     const guardarPaciente = async (paciente) => {
         try {
             const token = localStorage.getItem('token');
